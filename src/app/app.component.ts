b47116342d5ae4ff2d31aef23f84223ab6bd0750
Krishnakanth834 <krishnakanth834@gmail.com>
2018-10-17 15:19:22 +0530

    These done in the git repository it self.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
 }
-// This is krisjhnakanth latest changes.
+// This is krisjhnakanth latest changes.Changed in the git repository.
